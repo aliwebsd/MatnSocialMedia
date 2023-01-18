95d02cbc72930d98280f7e867147b35b2c769e9f
seyed ali davoodi <aliwebsd@gmail.com>
2023-01-19 02:56:15 +0330

    User Profile Article Ts Types

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+export interface User {
+  email: string;
+  token: string;
+  username: string;
+  bio: string;
+  image: string;
+}
+export interface Profile {
+  username: string;
+  bio: string;
+  image: string;
+  following: boolean;
+}
+export interface Article {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  /** @format date-time */
+  createdAt: string;
+  /** @format date-time */
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: Profile;
+}
