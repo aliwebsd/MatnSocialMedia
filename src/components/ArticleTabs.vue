32b7e30adea259e3182dbc451fd00aacda7a335f
seyed ali davoodi <aliwebsd@gmail.com>
2023-01-24 02:22:12 +0330

    fixed filter header bug

--- src/components/ArticleTabs.vue
+++ src/components/ArticleTabs.vue
@@ -24,29 +24,6 @@
           >
             <ArticleItem :article="article" />
           </div>
-          <!-- <Grid
-            :length="197"
-            :pageSize="articleLimit"
-            :pageProvider="pageProvider"
-            class="grid"
-          >
-            <template v-slot:probe>
-              <div class="item">Probe</div>
-            </template> -->
-
-          <!-- When the item is not loaded, a placeholder is rendered -->
-          <!-- <template v-slot:placeholder="{ index, style }">
-              <div class="item" :style="style">Placeholder {{ index }}</div>
-            </template> -->
-
-          <!-- Render a loaded item -->
-          <!-- <template v-slot:default="item"> -->
-          <!-- item, style, index -->
-          <!-- <div class="item" :style="style">{{ item }} {{ index }}</div> -->
-          <!-- {{ JSON.stringify(item) }} -->
-          <!-- <ArticleItem :article="item" /> -->
-          <!-- </template>
-          </Grid> -->
           <div class="p-4 text-center" v-if="loading">Loading articles...</div>
           <div
             class="p-4 text-center"
@@ -90,7 +67,9 @@ const { isAuthorized } = storeToRefs(useUserStore());
 
 const page = ref<number>(1);
 const headerFiltered = computed<ArticleTabHeader[]>(() =>
-  isAuthorized ? props.headers : props.headers.filter((i) => !i.requiresAuth)
+  isAuthorized.value
+    ? props.headers
+    : props.headers.filter((i) => !i.requiresAuth)
 );
 const tab = ref<string | null>(props.tab ?? headerFiltered.value[0].id);
 const articleLimit = ref<number>(props.limit ?? 10);
@@ -109,16 +88,4 @@ const totalPage = computed<number>(() =>
     ? Math.ceil(props.content.articlesCount / articleLimit.value)
     : 0
 );
-// const pageProvider = (pageNumber: number, pageSize: number) =>
-//   // eslint-disable-next-line no-async-promise-executor
-//   new Promise(async (resolve, reject) => {
-//     try {
-//       const { articles } = await getAll(isFeed.value, pageNumber, pageSize);
-//       debugger;
-//       resolve(articles);
-//     } catch (e) {
-//       console.log(e);
-//       reject(e);
-//     }
-//   });
 </script>
